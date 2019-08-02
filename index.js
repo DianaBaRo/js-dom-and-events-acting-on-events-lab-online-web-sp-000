848e4fca5014b23f059cdd8b66e26a29a9759ba2
Diana Badas <diana.badas@gmail.com>
2019-08-02 11:38:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,24 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+let input = document.querySelector('input');
+
+function retrieveEmployeeInformation() {
+  return input.value;
+}
+
+function addNewElementAsLi() {
+  let ul = document.querySelector('ul.employee-list');
+  let li = document.createElement('li');
+  li.innerHTML = retrieveEmployeeInformation();
+  ul.appendChild(li);
+}
+
+function addNewLiOnClick() {
+  let submit = document.querySelector('input[type="submit"]')
+  submit.addEventListener('click', function(e) {
+    addNewElementAsLi();
+    document.querySelector('input').value = '';
+  });
+}
